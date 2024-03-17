c145a27bb9ae00c6004e2e4847788c177381312b
gbw7942 <gbw7942@gmail.com>
2024-03-17 17:10:42 -0400

    Initial Commit

---
+++ ACTive/ACTiveApp.swift
@@ -0,0 +1,32 @@
+//
+//  ACTiveApp.swift
+//  ACTive
+//
+//  Created by Gabriel Wang on 3/17/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ACTiveApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
